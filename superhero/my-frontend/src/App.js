a2e43906afc1b2c340c0634d8267ef6a0c495c32
CryptoDebug <axel10.lemaitre@gmail.com>
2025-02-07 16:33:27 +0100

    ajout cases images + css

--- superhero/my-frontend/src/App.js
+++ superhero/my-frontend/src/App.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import AddHero from "./AddHero";
 import HeroDetail from "./HeroDetail";
+import "./App.css";
 
 const SuperheroesList = () => {
   const [heroes, setHeroes] = useState([]);
@@ -16,25 +17,35 @@ const SuperheroesList = () => {
         console.error("Erreur lors de la récupération des super-héros:", error);
       }
     };
-
     getHeroes();
   }, []);
 
   return (
-    <div>
-      <h1>Liste des Super-Héros</h1>
-      <ul>
+    <div className="container">
+      <header className="header">
+        <h1 className="logo">MyHeroLibrary</h1>
+        <input type="text" placeholder="Votre texte ici..." className="search-bar" />
+        <div className="profile-icon"></div>
+      </header>
+
+      <div className="filters">
+        <button>Planète</button>
+        <button>Pouvoir</button>
+        <button>Équipe</button>
+        <button>Ville</button>
+        <button>Sexe</button>
+      </div>
+
+      <div className="hero-grid">
         {heroes.map((hero) => (
-          <li key={hero.id}>
-            <Link to={`/hero/${hero.id}`}>
+          <div key={hero.id} className="hero-card">
+            <Link to={`/hero/${hero.id}`} className="hero-link">
+              <img src={hero.image} alt={hero.heroname} className="hero-image" />
               <span className="hero-name">{hero.heroname}</span>
             </Link>
-            <br />
-            <em>{hero.description}</em>
-          </li>
+          </div>
         ))}
-      </ul>
-      <AddHero setHeroes={setHeroes} />
+      </div>
     </div>
   );
 };
@@ -44,7 +55,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<SuperheroesList />} />
-        <Route path="/hero/:id" element={<HeroDetail />} /> {/* Ajout de la route pour HeroDetail */}
+        <Route path="/hero/:id" element={<HeroDetail />} />
       </Routes>
     </Router>
   );
